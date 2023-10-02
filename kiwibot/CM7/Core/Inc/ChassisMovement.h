3d17697dd7952951289203f3b1c969ed6f996928
fabianglz993 <a01634223@tec.mx>
2023-10-02 15:18:43 -0600

    Initial Setup

---
+++ kiwibot/CM7/Core/Inc/ChassisMovement.h
@@ -0,0 +1,21 @@
+/*
+ * ChassisMovement.h
+ *
+ *  Created on: Oct 2, 2023
+ *      Author: serfa
+ */
+
+#ifndef INC_CHASSISMOVEMENT_H_
+#define INC_CHASSISMOVEMENT_H_
+
+namespace LL_Control {
+
+class ChassisMovement {
+public:
+	ChassisMovement();
+	virtual ~ChassisMovement();
+};
+
+} /* namespace LL_Control */
+
+#endif /* INC_CHASSISMOVEMENT_H_ */
