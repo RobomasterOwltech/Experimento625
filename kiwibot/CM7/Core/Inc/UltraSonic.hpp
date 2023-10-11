2d8ef38e8cb7d177797095c1e4a7d6917c3241b3
sofiaariasv2002 <sofiaariasv2002@gmail.com>
2023-10-10 20:15:36 -0600

    Clase de ultrasonico

--- kiwibot/CM7/Core/Inc/UltraSonic.hpp
+++ kiwibot/CM7/Core/Inc/UltraSonic.hpp
@@ -4,14 +4,27 @@
  *  Created on: Oct 9, 2023
  *      Author: sofia
  */
-
+ 
 #ifndef INC_ULTRASONIC_HPP_
 #define INC_ULTRASONIC_HPP_
+
+#include "stm32h7xx_hal.h"
 
 class UltraSonic {
 public:
 	UltraSonic();
 	virtual ~UltraSonic();
+	float getDistance();
+	bool far(distance);
+	UltraSonic(float _distance, uint32_t _pMilis, TIM_HandleTypeDef *) 
+
+private:
+	float distance;
+	bool dataReceived;
+	//TIM_HandleTypeDef * timmer;
+	uint32_t pMillis = 0;
+    uint32_t Value1 = 0;
+    uint32_t Value2 = 0;
 };
 
 #endif /* INC_ULTRASONIC_HPP_ */
--- kiwibot/CM7/Core/Src/UltraSonic.cpp
+++ kiwibot/CM7/Core/Src/UltraSonic.cpp
@@ -9,10 +9,50 @@
 
 UltraSonic::UltraSonic() {
 	// TODO Auto-generated constructor stub
+	//inicializar
+	HAL_TIM_Base_Start(&timmer);
+    HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_RESET); // ponggo el trigger en bajo
 
 }
 
 UltraSonic::~UltraSonic() {
 	// TODO Auto-generated destructor stub
+}
+
+UltraSonic::UltraSonic(float dist, bool dataR, uint32_t pM, uint32_t V1, uint32_t V2){
+
+	distance=dist;
+	dataReceived=dataR;
+	pMillis=pM;
+	Value1=V1;
+	Value2=V2;
+} 
+
+float UltraSonic::getDistance(){
+	HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_SET);  // pongo el trigger el alto
+    __HAL_TIM_SET_COUNTER(&timmer, 0);
+    while (__HAL_TIM_GET_COUNTER (&timmer) < 10);  // espero 10 us
+        HAL_GPIO_WritePin(TRIG_PORT, TRIG_PIN, GPIO_PIN_RESET);  // pongo el trigger en bajo nuevamente.
+
+    pMillis = HAL_GetTick();
+    // espero que el ecco reciba el 1 del trigger
+    while (!(HAL_GPIO_ReadPin (ECHO_PORT, ECHO_PIN)) && pMillis + 10 >  HAL_GetTick());
+    Value1 = __HAL_TIM_GET_COUNTER (&timmer);
+
+    pMillis = HAL_GetTick();
+    // espero que el pin ecco este en bajo
+    while ((HAL_GPIO_ReadPin (ECHO_PORT, ECHO_PIN)) && pMillis + 50 > HAL_GetTick());
+        Value2 = __HAL_TIM_GET_COUNTER (&timmer);
+        Distance = (Value2-Value1)* 0.034/2;
+}
+
+bool UltraSonic::far(distance){
+	if (distance < 100){
+		dataReceived=false; //esta cerca de pared
+		return dataReceived; 
+	}else{
+		dataReceived=true; //esta lejos de pared
+		return dataReceived;
+	}
 }
 
